c5c9a83286ad400e5b6d56d0089757097d3efc6e
Jeroen Nijhuis <jeroen.nijhuis@justeattakeaway.com>
2024-01-11 21:34:03 +0100

    feat: added support for describing objects

---
+++ src/views/Describe.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Loading from "@/components/Loading.vue";
+import { Command } from "@tauri-apps/api/shell";
+
+const props = defineProps<{
+  context: string;
+  namespace: string;
+  object: string;
+}>();
+
+const describeContents = ref<string>("");
+
+onMounted(() => {
+  const command = new Command("kubectl", [
+    "describe",
+    props.object,
+    "--context",
+    props.context,
+    "--namespace",
+    props.namespace,
+  ]);
+
+  let stdOutData = "";
+  command.stdout.on("data", (data) => {
+    stdOutData += data;
+  });
+
+  command.on("close", ({ code }) => {
+    if (code === 0) {
+      describeContents.value = stdOutData;
+    }
+  });
+
+  command.spawn();
+});
+</script>
+<template>
+  <Loading label="loading..." v-if="describeContents.length === 0" />
+  <pre class="cursor-text select-text w-full h-full overflow-auto">{{
+    describeContents
+  }}</pre>
+</template>
