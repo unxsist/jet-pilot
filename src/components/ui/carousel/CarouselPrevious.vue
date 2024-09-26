d52f17c0c248c5fa108e1afb35feabcf670e6330
unxsist <jeroen@nijhuis.online>
2024-09-26 16:21:30 +0200

    chore: updated whats new for 1.30.0 release

---
+++ src/components/ui/carousel/CarouselPrevious.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from "@radix-icons/vue";
+import { useCarousel } from "./useCarousel";
+import type { WithClassAsProps } from "./interface";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const props = defineProps<WithClassAsProps>();
+
+const { orientation, canScrollPrev, scrollPrev } = useCarousel();
+</script>
+
+<template>
+  <Button
+    :disabled="!canScrollPrev"
+    :class="
+      cn(
+        'touch-manipulation absolute h-8 w-8 p-0',
+        orientation === 'horizontal'
+          ? '-left-12 top-1/2 -translate-y-1/2 ml-2'
+          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class
+      )
+    "
+    variant="default"
+    @click="scrollPrev"
+  >
+    <slot>
+      <ArrowLeftIcon class="h-4 w-4 text-current" />
+      <span class="sr-only">Previous Slide</span>
+    </slot>
+  </Button>
+</template>
---
+++ src/components/whats-new/1-30-0.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+</script>
+<template>
+  <Carousel :opts="{ loop: true }">
+    <CarouselContent>
+      <CarouselItem>
+        <div>
+          <img
+            src="@/assets/whats-new/helm-basic-support.gif"
+            alt="Basic Helm support"
+            class="w-full rounded-lg"
+          />
+          <h1 class="font-bold mt-5">Basic Helm support</h1>
+          <p class="text-sm">
+            You can now manage Helm releases. Currently only rollbacks and
+            deletion are supported. Support for installing and upgrading Helm
+            releases will be added in a future version.
+          </p>
+        </div>
+      </CarouselItem>
+      <CarouselItem>
+        <div>
+          <img
+            src="@/assets/whats-new/cluster-resources.gif"
+            alt="Cluster Objects"
+            class="w-full rounded-lg"
+          />
+          <h1 class="font-bold mt-5">Support for Cluster Objects</h1>
+          <p class="text-sm">
+            You can now view and manage cluster resources such as Nodes,
+            Namespaces and CRDs.
+          </p>
+        </div>
+      </CarouselItem>
+      <CarouselItem>
+        <div>
+          <img
+            src="@/assets/whats-new/cluster-events.gif"
+            alt="Events Support"
+            class="w-full rounded-lg"
+          />
+          <h1 class="font-bold mt-5">Support for Events</h1>
+          <p class="text-sm">
+            We've added a new Events view that allows you to see all the events
+            and the option to navigate to the involved resources quickly.
+          </p>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
+</template>
